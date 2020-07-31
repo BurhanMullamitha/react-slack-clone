88ca0da69e2e963424595da864a294aba2796ee0
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-07-31 03:31:35 +0100

    Setup Redux for the app, store logged in user to global app state

--- src/index.js
+++ src/index.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import * as serviceWorker from './serviceWorker';
-
-import "semantic-ui-css/semantic.min.css";
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import firebase from './firebase';
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route path="/" exact component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </Router>
-);
+import './index.css';
+import "semantic-ui-css/semantic.min.css";
+import rootReducer from './reducers';
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+    componentDidMount() {
+        console.log(this.props.isLoading);
+        firebase.auth().onAuthStateChanged(user => {
+            if(user) {
+                this.props.setUser(user);
+                this.props.history.push("/");
+            }
+        });
+    }
+    
+    render() {
+        return (
+            <Switch>
+                <Route path="/" exact component={App} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root));
 
 ReactDOM.render(
-    <Root />,
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth />
+        </Router>,
+    </Provider>,
     document.getElementById('root')
 );
 
---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+import * as actionTypes from '../actions/types';
+
+const initialUserState = {
+    currentUser: null,
+    isLoading: true
+};
+
+const users_reducer = (state = initialUserState, action) => {
+    switch(action.type) {
+        case actionTypes.SET_USER:
+            return {
+                currentUser: action.payload.currentUser,
+                isLoading: false
+            }
+        default:
+            return state;
+    }
+}
+
+const rootReducer = combineReducers({
+    user: users_reducer
+});
+
+export default rootReducer;
