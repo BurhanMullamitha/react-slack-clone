fc97a5b0998b70e270e830dbea15c6f27650c316
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-08-02 04:42:31 +0100

    Set first channel as active channel on load, removed channelRefs listeners on componentWillUnmount

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -6,28 +6,43 @@ import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
 
 class Channels extends React.Component {
     state = {
+        activeChannel: '',
         user: this.props.currentUser,
         channels: [],
         channelName: '',
         channelDetails: '',
         channelsRef: firebase.database().ref('channels'),
-        modal: false
+        modal: false,
+        firstLoad: true
     }
-    
-    openModal = () => this.setState({ modal: true });
-    
-    closeModal = () => this.setState({ modal: false });
     
     componentDidMount() {
         this.addListeners();
+    }
+    
+    componentWillUnmount() {
+        this.removeListeners();
     }
     
     addListeners = () => {
         let loadedChannels = [];
         this.state.channelsRef.on("child_added", snap => {
             loadedChannels.push(snap.val());
-            this.setState({ channels: loadedChannels });
+            this.setState({ channels: loadedChannels }, () => this.setFirstChannel());
         })
+    }
+    
+    removeListeners = () => {
+        this.state.channelsRef.off();
+    }
+    
+    setFirstChannel = () => {
+        const firstChannel = this.state.channels[0];
+        if(this.state.firstLoad && this.state.channels.length > 0) {
+            this.props.setCurrentChannel(firstChannel);
+            this.setActiveChannel(firstChannel);
+        }
+        this.setState({ firstLoad: false });
     }
     
     addChannel = () => {
@@ -51,7 +66,6 @@ class Channels extends React.Component {
             .then(() => {
                 this.setState({ channelName: '', channelDetails: '' });
                 this.closeModal();
-                console.log('Channel Added!');
             })
             .catch(err => {
                 console.log(err);
@@ -70,26 +84,33 @@ class Channels extends React.Component {
     }
     
     changeChannel = (channel) => {
+        this.setActiveChannel(channel);
         this.props.setCurrentChannel(channel);
     }
     
-    displayChannels = (channels) => (
+    setActiveChannel = (channel) => {
+        this.setState({ activeChannel: channel.id });
+    }
+    
+    displayChannels = (channels) =>
         channels.length > 0 && 
-            channels.map(channel => {
-                return (
-                    <Menu.Item
-                    key={channel.id}
-                    onClick={() => this.changeChannel(channel)}
-                    name={channel.name}
-                    style={{ opacity: 0.7 }}
-                    >
-                        # {channel.name}    
-                    </Menu.Item>
-                )
-            })
-    )
+            channels.map(channel => (
+                <Menu.Item
+                key={channel.id}
+                onClick={() => this.changeChannel(channel)}
+                name={channel.name}
+                style={{ opacity: 0.7 }}
+                active={channel.id === this.state.activeChannel}
+                >
+                    # {channel.name}    
+                </Menu.Item>
+    ));
     
     isFormValid = ({ channelName, channelDetails }) => channelName && channelDetails;
+    
+    openModal = () => this.setState({ modal: true });
+    
+    closeModal = () => this.setState({ modal: false });
     
     render() {
         const { channels, modal } = this.state;
