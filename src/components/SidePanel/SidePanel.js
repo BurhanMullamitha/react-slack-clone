d5f897f3b88848df1a5f90932b9a1d445ed4fe0d
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-08-05 23:05:13 +0100

    Added Starred Component to SidePanel for displaying favorite channels

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
+
 import UserPanel from './UserPanel';
 import Channels from './Channels';
 import DirectMessages from './DirectMessages';
-import { Menu } from 'semantic-ui-react';
+import Starred from './Starred';
 
 class SidePanel extends React.Component {
     render() {
@@ -16,6 +18,7 @@ class SidePanel extends React.Component {
                 style={{ backgroundColor: '#4c3c4c', fontSize: '1.2rem' }}
             >
                 <UserPanel currentUser={currentUser} />
+                <Starred />
                 <Channels currentUser={currentUser} />
                 <DirectMessages currentUser={currentUser} />
             </Menu>
