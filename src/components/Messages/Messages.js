766b3e15946d030630f74c13c5e0c1aeb3869425
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-08-07 19:53:58 +0100

    Added top posters of particular channel

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { setUserPosts } from '../../actions';
 import firebase from '../../firebase';
 import MessagesHeader from './MessagesHeader';
 import MessagesForm from './MessagesForm';
@@ -45,6 +47,7 @@ class Messages extends React.Component {
                 messagesLoading: false 
             });
             this.countUniqueUsers(loadedMessages);
+            this.countUserPosts(loadedMessages);
         });
     }
     
@@ -132,6 +135,21 @@ class Messages extends React.Component {
         this.setState({ numUniqueUsers });
     }
     
+    countUserPosts = (messages) => {
+        let userPosts = messages.reduce((acc, message) => {
+            if(message.user.name in acc) {
+                acc[message.user.name].count += 1;
+            } else {
+                acc[message.user.name] = {
+                    avatar: message.user.avatar,
+                    count: 1
+                };
+            }
+            return acc;
+        }, {});
+        this.props.setUserPosts(userPosts);
+    }
+    
     displayMessages = (messages) => (
         messages.length > 0 && messages.map(message => (
             <Message 
@@ -179,4 +197,4 @@ class Messages extends React.Component {
     }
 }
 
-export default Messages;
+export default connect(null, { setUserPosts })(Messages);
